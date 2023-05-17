38aade97f0055570e19596bd35c42a470dd658bc
Sohaib6266 <131818777+Sohaib6266@users.noreply.github.com>
2023-05-17 18:52:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// //========variables=========//
+// var x = 10;
+// var y = 20;
+// var z = x+y;
+
+// console.log(z);
+
+// var firstname = prompt("Enter name");
+// var lastname = prompt("Enter lastname");
+// var city = prompt("Enter City");
+// console.log(firstname +" " + lastnme + " " + city);
+
+// ============pre-increment============//
+
+// var a =12;
+// console.log(b);
+// var b = ++a;
+
+// ============if & else statement============//
+//   var = january = true;
+//    if(january == true){
+//     console.log("correct");
+//    }
+//    else{
+//     console.log("wrong");
+//    }
+
+
+//==========PERCENTAGE CALCULATOR==============//
+
+// var maths = +prompt("enter maths number");
+// var english = +prompt("enter english number");
+// var urdu = +prompt("enter urdu number");
+// var physics = +prompt("enter physics number");
+// var computer = +prompt("enter computer number");
+
+// var obtainedmarks = maths + english + urdu + physics + computer;
+// var totalmarks = 500;
+// var percentage = obtainedmarks / totalmarks * 100;
+//     console.log(percentage)
+
+//=========ARRAYS=========//
+
+// var players = ["Babar","Rizwan","Shaheen","shadab","Rauf"];
+
+//     console.log(players)
+
+
+
